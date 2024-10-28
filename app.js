156a4f1eab3a638b738741af7af07f3e2afcf11d
roman deblase <rdeblase2000@gmail.com>
2024-10-27 22:48:14 -0400

    Implement shape drawing logic for lines, rectangles, and circles.

--- app.js
+++ app.js
@@ -16,3 +16,42 @@ function startDrawing(e) {
     startY = e.offsetY;
 }
    
+function draw(e) {
+    if (!isDrawing) return;
+
+    const selectedTool = document.querySelector('input[name="tool"]:checked').value;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+
+    ctx.beginPath();
+
+    //line tool
+    if (selectedTool === 'line') {
+        
+        ctx.moveTo(startX, startY);
+        ctx.lineTo(e.offsetX, e.offsetY);
+    } 
+    //rectangle
+    else if (selectedTool === 'rectangle') {
+        
+        const width = e.offsetX - startX;
+        const height = e.offsetY - startY;
+        ctx.strokeRect(startX, startY, width, height);
+    } 
+    //circle
+    else if (selectedTool === 'circle') {
+        
+        const radius = Math.sqrt(
+            Math.pow(e.offsetX - startX, 2) + 
+            Math.pow(e.offsetY - startY, 2)
+        );
+        ctx.arc(startX, startY, radius, 0, 2 * Math.PI);
+    }
+
+    ctx.stroke();
+}
+
+function stopDrawing() {
+    isDrawing = false
+}
